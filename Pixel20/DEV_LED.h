5e770f6c4461b2cd3ca54df33c7ee87dd3c11487
Rob-O-Code <40807772+Rob-O-Code@users.noreply.github.com>
2023-09-17 13:37:45 -0400

    Initial commit, working bridge demo

---
+++ Pixel20/DEV_LED.h
@@ -0,0 +1,232 @@
+
+////////////////////////////////////
+//   DEVICE-SPECIFIC LED SERVICES //
+////////////////////////////////////
+
+#include "extras/PwmPin.h"                          // library of various PWM functions
+#include "HomeSpan.h" 
+#include <Adafruit_NeoPixel.h>
+
+////////////////////////////////////
+
+struct DEV_LED : Service::LightBulb {               // ON/OFF LED
+
+  int ledPin;                                       // pin number defined for this LED
+  SpanCharacteristic *power;                        // reference to the On Characteristic
+  
+  DEV_LED(int ledPin) : Service::LightBulb(){       // constructor() method
+
+    power=new Characteristic::On();                 
+    this->ledPin=ledPin;                            
+    pinMode(ledPin,OUTPUT);                         
+    
+    Serial.print("Configuring On/Off LED: Pin=");   // initialization message
+    Serial.print(ledPin);
+    Serial.print("\n");
+
+  } // end constructor
+
+  boolean update(){                              // update() method
+
+    LOG1("Updating On/Off LED on pin=");
+    LOG1(ledPin);
+    LOG1(":  Current Power=");
+    LOG1(power->getVal()?"true":"false");
+    LOG1("  New Power=");
+    LOG1(power->getNewVal()?"true":"false");
+    LOG1("\n");
+
+    digitalWrite(ledPin,power->getNewVal());      
+   
+    return(true);                               // return true
+  
+  } // update
+};
+      
+//////////////////////////////////
+
+struct DEV_DimmableLED : Service::LightBulb {       // Dimmable LED
+
+  LedPin *ledPin;                                   // reference to Led Pin
+  SpanCharacteristic *power;                        // reference to the On Characteristic
+  SpanCharacteristic *level;                        // reference to the Brightness Characteristic
+  
+  DEV_DimmableLED(int pin) : Service::LightBulb(){       // constructor() method
+
+    power=new Characteristic::On();     
+                
+    level=new Characteristic::Brightness(50);       // Brightness Characteristic with an initial value of 50%
+    level->setRange(5,100,1);                       // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
+
+    this->ledPin=new LedPin(pin);                   // configures a PWM LED for output to the specified pin
+
+    Serial.print("Configuring Dimmable LED: Pin="); // initialization message
+    Serial.print(ledPin->getPin());
+    Serial.print("\n");
+    
+  } // end constructor
+
+  boolean update(){                              // update() method
+
+    LOG1("Updating Dimmable LED on pin=");
+    LOG1(ledPin->getPin());
+    LOG1(":  Current Power=");
+    LOG1(power->getVal()?"true":"false");
+    LOG1("  Current Brightness=");
+    LOG1(level->getVal());
+ 
+    if(power->updated()){
+      LOG1("  New Power=");
+      LOG1(power->getNewVal()?"true":"false");
+    }
+
+    if(level->updated()){
+      LOG1("  New Brightness=");
+      LOG1(level->getNewVal());
+    } 
+
+    LOG1("\n");
+    
+    ledPin->set(power->getNewVal()*level->getNewVal());    
+   
+    return(true);                               // return true
+  
+  } // update
+};
+      
+//////////////////////////////////
+
+struct DEV_RgbLED : Service::LightBulb {       // RGB LED (Command Cathode)
+
+  Adafruit_NeoPixel strip;
+  
+  SpanCharacteristic *power;                   // reference to the On Characteristic
+  SpanCharacteristic *H;                       // reference to the Hue Characteristic
+  SpanCharacteristic *S;                       // reference to the Saturation Characteristic
+  SpanCharacteristic *V;                       // reference to the Brightness Characteristic
+  
+  DEV_RgbLED(int red_pin, int green_pin, int blue_pin) : Service::LightBulb(){       // constructor() method
+
+    power=new Characteristic::On();                    
+    H=new Characteristic::Hue(0);              // instantiate the Hue Characteristic with an initial value of 0 out of 360
+    S=new Characteristic::Saturation(0);       // instantiate the Saturation Characteristic with an initial value of 0%
+    V=new Characteristic::Brightness(100);     // instantiate the Brightness Characteristic with an initial value of 100%
+    V->setRange(5,100,1);                      // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
+    
+    // this->redPin=new LedPin(red_pin);        // configures a PWM LED for output to the RED pin
+    // this->greenPin=new LedPin(green_pin);    // configures a PWM LED for output to the GREEN pin
+    // this->bluePin=new LedPin(blue_pin);      // configures a PWM LED for output to the BLUE pin
+    strip = Adafruit_NeoPixel(3, MOSI, NEO_GRB + NEO_KHZ800);
+
+    strip.begin();
+    strip.show();
+    strip.setBrightness(50);
+
+    strip.setPixelColor(0, strip.Color(0, 127, 255));
+ 
+    // char cBuf[128];
+    // sprintf(cBuf,"Configuring RGB LED: Pins=(%d,%d,%d)\n",redPin->getPin(),greenPin->getPin(),bluePin->getPin());
+    // Serial.print(cBuf);
+    
+  } // end constructor
+
+  boolean update(){                         // update() method
+
+    boolean p;
+    float v, h, s, r, g, b;
+
+    h=H->getVal<float>();                      // get and store all current values.  Note the use of the <float> template to properly read the values
+    s=S->getVal<float>();
+    v=V->getVal<float>();                      // though H and S are defined as FLOAT in HAP, V (which is brightness) is defined as INT, but will be re-cast appropriately
+    p=power->getVal();
+
+    char cBuf[128];
+    sprintf(cBuf,"Updating RGB");
+    //sprintf(cBuf,"Updating RGB LED: Pins=(%d,%d,%d): ",redPin->getPin(),greenPin->getPin(),bluePin->getPin());
+    LOG1(cBuf);
+
+    if(power->updated()){
+      p=power->getNewVal();
+      sprintf(cBuf,"Power=%s->%s, ",power->getVal()?"true":"false",p?"true":"false");
+    } else {
+      sprintf(cBuf,"Power=%s, ",p?"true":"false");
+    }
+    LOG1(cBuf);
+      
+    if(H->updated()){
+      h=H->getNewVal<float>();
+      sprintf(cBuf,"H=%.0f->%.0f, ",H->getVal<float>(),h);
+    } else {
+      sprintf(cBuf,"H=%.0f, ",h);
+    }
+    LOG1(cBuf);
+
+    if(S->updated()){
+      s=S->getNewVal<float>();
+      sprintf(cBuf,"S=%.0f->%.0f, ",S->getVal<float>(),s);
+    } else {
+      sprintf(cBuf,"S=%.0f, ",s);
+    }
+    LOG1(cBuf);
+
+    if(V->updated()){
+      v=V->getNewVal<float>();
+      sprintf(cBuf,"V=%.0f->%.0f  ",V->getVal<float>(),v);
+    } else {
+      sprintf(cBuf,"V=%.0f  ",v);
+    }
+    LOG1(cBuf);
+
+    // Here we call a static function of LedPin that converts HSV to RGB.
+    // Parameters must all be floats in range of H[0,360], S[0,1], and V[0,1]
+    // R, G, B, returned [0,1] range as well
+
+    // LedPin::HSVtoRGB(h,s/100.0,v/100.0,&r,&g,&b);   // since HomeKit provides S and V in percent, scale down by 100
+
+    // int R, G, B;
+
+    // R=p*r*100;                                      // since LedPin uses percent, scale back up by 100, and multiple by status fo power (either 0 or 1)
+    // G=p*g*100;
+    // B=p*b*100;
+
+    // sprintf(cBuf,"RGB=(%d,%d,%d)\n",R,G,B);
+    // LOG1(cBuf);
+
+    // redPin->set(R);                      // update each ledPin with new values
+    // greenPin->set(G);    
+    // bluePin->set(B);   
+
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, strip.ColorHSV(h * 65535UL / 360, s * 255 / 100, v * 255 / 100));
+    } 
+    strip.show();
+      
+    return(true);                               // return true
+  
+  } // update
+};
+      
+//////////////////////////////////
+
+struct DEV_HeatFan : Service::HeaterCooler {
+
+    SpanCharacteristic *active;
+    SpanCharacteristic *currentTemp;
+    SpanCharacteristic *currentState;
+    SpanCharacteristic *targetState;
+    SpanCharacteristic *heatThresh;
+
+    DEV_HeatFan() : Service::HeaterCooler() {
+        active = new Characteristic::Active();
+        currentTemp = new Characteristic::CurrentTemperature(30);
+        currentState = new Characteristic::CurrentHeaterCoolerState();
+        targetState = new Characteristic::TargetHeaterCoolerState();
+        heatThresh = new Characteristic::HeatingThresholdTemperature(40);
+    }
+
+    boolean update() {
+
+        
+        return true;
+    }
+};
